e7ca8fc22d929bf5f424581442568fbd42710bd3
giray104 <giray104@yahoo.com>
2017-05-08 23:13:34 +0300

    test_getMostLikedPages

--- apiexample/twitter/tests.py
+++ apiexample/twitter/tests.py
@@ -18,3 +18,12 @@ class StatsTestCases(TestCase):
     	self.assertEqual(status_code,2)
     	json = r.json()
     	self.assertEqual(count,json['count'])
+ 	
+ 	# Unit Test for getMostLikedPages method
+ 	# It runs the function on a twitter account that is reserved for test purposes 
+ 	# and compares expected output with produced one.
+    def test_getMostLikedPages(self):
+    	count = 100
+    	parameters = {'username': 'menescakir', 'count': count}
+    	r = requests.get('http://127.0.0.1:8000/user/liked', params=parameters)
+    	self.assertEqual(r.text,"(2280834239, \"Leyla'dan Sonra\")(2282508158, 'Biryudumkitap')(4048169297, 'Rumeysa Kadak')")
