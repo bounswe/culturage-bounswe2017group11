a818a5e7e867c87075257b14c034f21bb95213f7
rizaozcelik <rizaozcelik96@gmail.com>
2017-05-08 14:07:42 +0300

    Unit test is written for my own method

--- apiexample/twitter/tests.py
+++ apiexample/twitter/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import unittest
+import requests
+import json
+from pprint import pprint
 
-# Create your tests here.
+class StatsTestCases(TestCase):
+ 	
+ 	# Unit Test for getFrequencyOfWordsOfLikedTweets method
+ 	# It tests two things, status code of the request and number of tweets searched 
+ 	# during execution
+    def test_getFrequencyOfWordsOfLikedTweets(self):
+    	count = 100
+    	parameters = {'username': 'menescakir', 'count': count}
+    	r = requests.get('http://127.0.0.1:8000/user/favwords', params=parameters)
+    	#print(r.GET.get('username'))
+    	status_code = r.status_code // 100
+    	self.assertEqual(status_code,2)
+    	json = r.json()
+    	self.assertEqual(count,json['count'])
--- apiexample/twitter/urls.py
+++ apiexample/twitter/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^example', views.example, name='example'),
-    url(r'^user/favwords', views.getFrequencyOfWordsOfLikedTweets, name='getFrequencyOfWordsOfLikedTweets'),
-    url(r'^followers/freq', views.getUsersTweetingMostFrequently, name='getUsersTweetingMostFrequently'),
-    url(r'^followers/popular', views.getMostNumberOfFollowers, name='getMostNumberOfFollowers'),
-    url(r'^user/liked', views.getMostLikedPages, name='getMostLikedPages'),
-    url(r'^user/mention', views.getWhoMentionedMost, name='getWhoMentionedMost'),
-    url(r'^user/allwords', views.getFrequencyOfWordsOfAllTweets, name='getFrequencyOfWordsOfAllTweets'),
-    url(r'^users/likes', views.getLikeRatioOfTwoUsers, name='getLikeRatioOfTwoUsers'),
-    url(r'^user/hashtag', views.hashtagPercentage, name='hashtagPercentage')
+    url(r'^$', views.TwitterStats.index, name='index'),
+    url(r'^example', views.TwitterStats.example, name='example'),
+    url(r'^user/favwords', views.TwitterStats.getFrequencyOfWordsOfLikedTweets, name='getFrequencyOfWordsOfLikedTweets'),
+    url(r'^followers/freq', views.TwitterStats.getUsersTweetingMostFrequently, name='getUsersTweetingMostFrequently'),
+    url(r'^followers/popular', views.TwitterStats.getMostNumberOfFollowers, name='getMostNumberOfFollowers'),
+    url(r'^user/liked', views.TwitterStats.getMostLikedPages, name='getMostLikedPages'),
+    url(r'^user/mention', views.TwitterStats.getWhoMentionedMost, name='getWhoMentionedMost'),
+    url(r'^user/allwords', views.TwitterStats.getFrequencyOfWordsOfAllTweets, name='getFrequencyOfWordsOfAllTweets'),
+    url(r'^users/likes', views.TwitterStats.getLikeRatioOfTwoUsers, name='getLikeRatioOfTwoUsers'),
+    url(r'^user/hashtag', views.TwitterStats.hashtagPercentage, name='hashtagPercentage')
 ]
