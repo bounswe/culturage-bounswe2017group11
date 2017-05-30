5aebe824f72b0992e14463919ea1c25afe255eb4
A Koksal <koksalyazilim@gmail.com>
2017-05-30 13:14:10 +0300

    Documentation is added

--- apiexample/twitter/html/search/all_2.js
+++ apiexample/twitter/html/search/all_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['twitterconfig',['TwitterConfig',['../classtwitter_1_1apps_1_1_twitter_config.html',1,'twitter::apps']]],
-  ['twitterstats',['TwitterStats',['../classtwitter_1_1views_1_1_twitter_stats.html',1,'twitter::views']]]
+  ['statstestcases',['StatsTestCases',['../classtwitter_1_1tests_1_1_stats_test_cases.html',1,'twitter::tests']]]
 ];
