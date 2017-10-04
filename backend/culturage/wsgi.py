a1ad3b14dfc274c32eddebcfb4ddbaef8d80273a
Enes Çakır <enes@cakir.web.tr>
2017-10-04 22:03:23 +0300

    Add importing forgetten sys lib

--- backend/culturage/wsgi.py
+++ backend/culturage/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
