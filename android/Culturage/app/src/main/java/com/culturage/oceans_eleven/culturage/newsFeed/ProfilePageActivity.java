fd729c29cde4304b9c533ec227026533cb5c1059
Barın Özmen <ozmenbrn@gmail.com>
2017-10-17 23:16:53 +0300

    Update on profile page

--- android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/ProfilePageActivity.java
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/ProfilePageActivity.java
@@ -1,37 +1,93 @@
 package com.culturage.oceans_eleven.culturage.newsFeed;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.culturage.oceans_eleven.culturage.R;
+import com.culturage.oceans_eleven.culturage.signup_login.LoginActivity;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class ProfilePageActivity extends AppCompatActivity {
+
+    private int SELECT_FILE = 1;
+    private ImageView profileImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        ArrayList<String> list = new ArrayList<String>();
+        Button uploadProfilePic = (Button) findViewById(R.id.uploadProfilePic);
+        uploadProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //userChosenTask = "Choose from Gallery";
+                galleryIntent();
+            }
+        });
 
-        list.add("Name");
-        list.add("Surname");
-        list.add("Username");
-        list.add("Date Of Birth");
-        list.add("Email Address");
-        list.add("About You");
+        profileImage = (ImageView) findViewById(R.id.CulturagePicture);
 
+        Button signout = (Button) findViewById(R.id.signOut);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfilePageActivity.this, LoginActivity.class));
+            }
+        });
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        final boolean isValidProfileChange = true;
+        Button saveprofile = (Button) findViewById(R.id.saveProfile);
+        saveprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isValidProfileChange) {
+                    startActivity(new Intent(ProfilePageActivity.this, NewsFeedActivity.class));
+                }
+            }
+        });
 
-        ListView listView = (ListView) findViewById(R.id.profilePage_);
+    }
 
-        listView.setAdapter(itemsAdapter);
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE) {
+                onSelectFromGalleryResult(data);
+            }
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        profileImage.setImageBitmap(bm);
 
     }
 
