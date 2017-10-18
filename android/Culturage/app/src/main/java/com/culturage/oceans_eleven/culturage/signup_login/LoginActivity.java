bd2775e57af4aef889d0ddb7603c566bd60e147c
rizaozcelik <rizaozcelik96@gmail.com>
2017-10-18 14:17:26 +0300

    Force internet connection in app start-up

--- android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/signup_login/LoginActivity.java
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/signup_login/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.culturage.oceans_eleven.culturage.signup_login;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,7 +19,6 @@ import com.culturage.oceans_eleven.culturage.newsFeed.NewsFeedActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // Barin
     private EditText mUsernameView;
     private EditText mPasswordView;
 
@@ -22,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        forceInternetConnection();
+
         // Set up the login form.
         mUsernameView = (EditText) findViewById(R.id.username);
 
@@ -44,6 +50,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void forceInternetConnection() {
+        ConnectivityManager conMan = (ConnectivityManager) LoginActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conMan.getActiveNetworkInfo() == null || !conMan.getActiveNetworkInfo().isConnected()) {
+            AlertDialog.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                builder = new AlertDialog.Builder(LoginActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                builder = new AlertDialog.Builder(LoginActivity.this);
+            }
+            builder.setTitle("No Internet Connection")
+                    .setMessage("Please connect to the internet")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            //finish();
+        }
     }
 
 
