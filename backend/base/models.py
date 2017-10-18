c6505adf551ec3ac2a1201efabfc75a84e0a3507
otugen <nhlezgiyctrk@gmail.com>
2017-10-18 11:36:55 +0300

    Added Comment model

--- backend/base/models.py
+++ backend/base/models.py
@@ -10,3 +10,11 @@ class Item(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='items_created', on_delete=models.SET_NULL, null=True)
     updated_by = models.ForeignKey(User, related_name='items_updated', on_delete=models.SET_NULL, null=True)
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    rate = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    written_by = models.ForeignKey(User, related_name='comment_written', on_delete=models.SET_NULL, null=True)
+    related_item = models.ForeignKey(Item, related_name='related_item', on_delete=models.SET_NULL, null=True)
