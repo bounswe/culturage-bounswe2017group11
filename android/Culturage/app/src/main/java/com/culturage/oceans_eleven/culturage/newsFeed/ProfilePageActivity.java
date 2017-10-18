9f41a30303e44ec19119a66c08883d802356e418
Barın Özmen <ozmenbrn@gmail.com>
2017-10-18 23:47:28 +0300

    Profile Page is completed. Yet upload picture and save changes are not working well

--- android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/ProfilePageActivity.java
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/ProfilePageActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.culturage.oceans_eleven.culturage.R;
 import com.culturage.oceans_eleven.culturage.signup_login.LoginActivity;
@@ -26,6 +28,7 @@ public class ProfilePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
+        //Upload profile picture button
         Button uploadProfilePic = (Button) findViewById(R.id.uploadProfilePic);
         uploadProfilePic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +40,7 @@ public class ProfilePageActivity extends AppCompatActivity {
 
         profileImage = (ImageView) findViewById(R.id.CulturagePicture);
 
-        Button signout = (Button) findViewById(R.id.signOut);
+        Button signout = (Button) findViewById(R.id.SignOut);
         signout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +48,7 @@ public class ProfilePageActivity extends AppCompatActivity {
             }
         });
 
+        //Save Change button for saving profile changes
         final boolean isValidProfileChange = true;
         Button saveprofile = (Button) findViewById(R.id.saveProfile);
         saveprofile.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +60,38 @@ public class ProfilePageActivity extends AppCompatActivity {
             }
         });
 
+
+        Spinner spinner = (Spinner) findViewById(R.id.birthDaySpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.birthDayList, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        spinner = (Spinner) findViewById(R.id.birthMonthSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.birthMonthList, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        spinner = (Spinner) findViewById(R.id.birthYearSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.birthYearList, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
 
     private void galleryIntent() {
         Intent intent = new Intent();
