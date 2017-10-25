7607c0bf1c67fead44b50843f84f4999cd3ffeb2
hilalbenzer <hilalbenzer@gmail.com>
2017-10-25 21:51:27 +0300

    edit navbar

--- frontend/app/pages/Home/components/NewsFeedItem.jsx
+++ frontend/app/pages/Home/components/NewsFeedItem.jsx
@@ -12,7 +12,7 @@ class NewsFeedItem extends React.Component {
                               <div class="col-sm-6">
                                  <a href="#"><h1>{this.props.item.name}</h1></a>
                                  <h2>{this.props.item.created_at.substring(0, 10)} - {this.props.item.created_by.username}</h2>
-                                 <p>{this.props.item.description.substring(0, 200)}...<a href="#">Read more..</a></p>
+                                 <p>{this.props.item.description.substring(0, 200)}...<a href="/item">Read more..</a></p>
                                  
                               </div>
                               <div class="col-sm-6">
