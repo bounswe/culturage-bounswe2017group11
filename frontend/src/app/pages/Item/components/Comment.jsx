bedadd1d5d66ad332e9f28201aa8d9128cdbfb2a
Halil <halilkalkan95@gmail.com>
2017-10-27 03:23:12 +0300

    css styling is improved

--- frontend/src/app/pages/Item/components/Comment.jsx
+++ frontend/src/app/pages/Item/components/Comment.jsx
@@ -10,23 +10,23 @@ class Comment extends React.Component {
    if(typeof this.props.item.created_by != "undefined") {
     comment = <div>
         <div class="mycontainer2">
-        <div class="panel panel-white post">
-            <div class="card-header text-white bg-info">
+        <div class="panel panel-info">
+            <div class="panel-heading">
                 Item details
                 </div>
-            <div class="card-body">
-            <h5 class="card-title">Id: {this.props.item.id}</h5>
-            <h5 class="card-title">Created by: {this.props.item.created_by.username}</h5>
+            <div class="panel-body">
+            <h5 class="panel-title"><strong>Id:</strong> {this.props.item.id}</h5>
+            <h5 class="panel-title"><strong>Created by:</strong> {this.props.item.created_by.username}</h5>
 
-            <h5 class="card-title">Created at: {this.props.item.created_at.substring(0,10)}</h5>
+            <h5 class="panel-title"><strong>Created at:</strong> {this.props.item.created_at.substring(0,10)}</h5>
 
             </div>
         </div>
          
         <br />
 
-        <div class="panel panel-white post">
-            <div class="card-header text-white bg-info">
+        <div class="panel panel-info post">
+            <div class="panel-heading">
                 Comments
             </div>
             <div class="post-footer">
@@ -37,6 +37,18 @@ class Comment extends React.Component {
                     </span>
                 </div>
                 <ul class="comments-list">
+                    <li class="comment">
+                        <a class="pull-left" href="#">
+                            <img class="avatar" src="http://bootdey.com/img/Content/user_1.jpg" alt="avatar"/>
+                        </a>
+                        <div class="comment-body">
+                            <div class="comment-heading">
+                                <h4 class="user">Canberk</h4>
+                                <h5 class="time">5 minutes ago</h5>
+                            </div>
+                            <p>Wow! Hilarious. </p>
+                        </div>
+                    </li>
                     <li class="comment">
                         <a class="pull-left" href="#">
                             <img class="avatar" src="http://bootdey.com/img/Content/user_1.jpg" alt="avatar"/>
