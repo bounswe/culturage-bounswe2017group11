dd47f372201ff5a487b8e822ed72009b49c5a701
rizaozcelik <rizaozcelik96@gmail.com>
2017-11-10 14:16:22 +0300

    Comment dialog code cleaning.

--- android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/CustomDialogClass.java
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/CustomDialogClass.java
@@ -1,20 +1,15 @@
 package com.culturage.oceans_eleven.culturage.newsFeed;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.culturage.oceans_eleven.culturage.R;
@@ -24,42 +19,37 @@ import java.util.ArrayList;
 public class CustomDialogClass extends Dialog implements
         android.view.View.OnClickListener {
 
-    public Activity activity;
-    public Dialog dialog;
+    //    private final String LOG_TAG = "dialog_tag";
+    private Activity activity;
 
-    private final String LOG_TAG = "dialog_tag";
-    Button showMore, showComments;
-    private static CommentAdapter commentAdapter;
-    ListView listView;
-    TextView commentContent, commentOwner;
-
-    public CustomDialogClass(Activity activity) {
+    CustomDialogClass(Activity activity) {
         super(activity);
-        // TODO Auto-generated constructor stub
         this.activity = activity;
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.custom_dialog);
+
         Rect displayRectangle = new Rect();
         Window window = activity.getWindow();
         window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
-
+    /* Set the size of the dialog */
         ScrollView container = (ScrollView) findViewById(R.id.comment_container);
 //        container.getLayoutParams().height = (int)(displayRectangle.height() * 0.9f);
         container.getLayoutParams().width = (int) (displayRectangle.width() * 0.98f);
         container.getLayoutParams().height = (int) (displayRectangle.height() * 0.98f);
         container.requestLayout();
+        /* Set the size of the list view */
+        final ListView commentLV = (ListView) findViewById(R.id.comment_list);
+        commentLV.getLayoutParams().height = displayRectangle.width();
+        commentLV.requestLayout();
+        /* These lines are to handle scrolling of list view inside the scroll view */
+        commentLV.setScrollingCacheEnabled(false);
 
-        final ListView lv = (ListView) findViewById(R.id.comment_list);
-        lv.getLayoutParams().height = (int) (displayRectangle.width());
-        lv.requestLayout();
-        lv.setScrollingCacheEnabled(false);
-        lv.setOnTouchListener(new View.OnTouchListener() {
+        commentLV.setOnTouchListener(new View.OnTouchListener() {
             // Setting on Touch Listener for handling the touch inside ScrollView
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -67,6 +57,7 @@ public class CustomDialogClass extends Dialog implements
                 v.getParent().requestDisallowInterceptTouchEvent(true);
                 return false;
             }
+
         });
 
         ImageButton uploadCommentBtn = (ImageButton) findViewById(R.id.upload_comment);
@@ -94,9 +85,9 @@ public class CustomDialogClass extends Dialog implements
                 dismiss();
             }
         });
-        Log.v(LOG_TAG, "before adapter init");
-        ArrayList<Comment> comments = new ArrayList<Comment>();
-        commentAdapter = new CommentAdapter(activity, comments);
+
+        ArrayList<Comment> comments = new ArrayList<>();
+        CommentAdapter commentAdapter = new CommentAdapter(activity, comments);
         comments.add(new Comment("Wow amazing asdasdsadasdsadsadsadadsadad asdsadasd asdasdasd adasdsadsa asdasd sa dasdasd sa ad sadas aasadsasd sdjasdnasd ddjfafad fdg dsfg sdfg nsdf sd fdg dsfg sdf gsdf sdg sfg ", "rza", "http://www.warmanchamber.com/wp-content/uploads/2016/10/person-placeholder-image-3.jpg"));
         comments.add(new Comment("Great pic", "someone", "http://www.warmanchamber.com/wp-content/uploads/2016/10/person-placeholder-image-3.jpg"));
 //        comments.add(new Comment("not that cool", "some other", "http://www.warmanchamber.com/wp-content/uploads/2016/10/person-placeholder-image-3.jpg"));
@@ -108,42 +99,10 @@ public class CustomDialogClass extends Dialog implements
         comments.add(new Comment("not that cool", "some other", "http://www.warmanchamber.com/wp-content/uploads/2016/10/person-placeholder-image-3.jpg"));
         comments.add(new Comment("not that cool", "some other", "http://www.warmanchamber.com/wp-content/uploads/2016/10/person-placeholder-image-3.jpg"));
 //        listView = (ListView) findViewById(R.id.comment_list);
-        lv.setAdapter(commentAdapter);
-        for (Comment c : comments) {
-            Log.v(LOG_TAG, c.getContent());
-        }
-//        commentAdapter.clear();
-//        if (comments != null || comments.size() > 0) {
-//        commentAdapter.addAll(comments);
-//        }
-//        if (comments.size() > 0) {
-
-//        }
-        Log.v(LOG_TAG, "after listener" + comments.size());
+        commentLV.setAdapter(commentAdapter);
     }
 
     @Override
     public void onClick(View v) {
-    }
-
-    public static void setListViewHeightBasedOnChildren(ListView listView) {
-        CommentAdapter listAdapter = (CommentAdapter) listView.getAdapter();
-        if (listAdapter == null)
-            return;
-
-        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-        int totalHeight = 0;
-        View view = null;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            view = listAdapter.getView(i, view, listView);
-            if (i == 0)
-                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-            totalHeight += view.getMeasuredHeight();
-        }
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
     }
 }
