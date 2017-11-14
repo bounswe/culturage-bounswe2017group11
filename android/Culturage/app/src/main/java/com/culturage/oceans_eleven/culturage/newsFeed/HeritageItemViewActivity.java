d8a25901b6a7e8d41bc640ef279b9ed806b412b8
ozmenbrn <ozmenbrn@gmail.com>
2017-11-15 01:07:17 +0300

    On heritage item page, implemented 2 loaders, one is for guest profile picture, the other will be slider for recomended heritage items.

--- android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/HeritageItemViewActivity.java
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/newsFeed/HeritageItemViewActivity.java
@@ -1,11 +1,14 @@
 package com.culturage.oceans_eleven.culturage.newsFeed;
 
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -13,14 +16,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.culturage.oceans_eleven.culturage.R;
+import com.culturage.oceans_eleven.culturage.adapters.HeritageItemAdapter;
 import com.culturage.oceans_eleven.culturage.baseClasses.CustomDialogClass;
+import com.culturage.oceans_eleven.culturage.baseClasses.ProfilePage;
+import com.culturage.oceans_eleven.culturage.network.ProfilePageLoader;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class HeritageItemViewActivity extends AppCompatActivity {
 
     private int heritageItemPostID = -1;
     private String creator_id;
     private String creator_username;
+    private ImageView photo;
+    private String profileURL = "http://18.220.108.135/api/profile/";
+    private static final String itemUrl = "http://18.220.108.135/api/items";
+
+    private HeritageItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,10 +107,15 @@ public class HeritageItemViewActivity extends AppCompatActivity {
         TextView tags = (TextView) findViewById(R.id.her_item_tags);
         tags.setText(getIntent().getStringExtra("tags"));
 
+
+        /**
+         * Guest User Profile
+         */
         creator_id = getIntent().getStringExtra("creator_id");
         creator_username = getIntent().getStringExtra("creator_username");
         TextView guest = (TextView) findViewById(R.id.guest_profile);
-        guest.setText(creator_username);
+        ImageView guestPic = (ImageView) findViewById(R.id.guest_profile_pict);
+        guest.setText(" " + creator_username);
         guest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,8 +125,87 @@ public class HeritageItemViewActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        guestPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HeritageItemViewActivity.this, ProfileGuestActivity.class));
+                Intent intent = new Intent(HeritageItemViewActivity.this, ProfileGuestActivity.class);
+                intent.putExtra("creator_id", creator_id); //New
+                startActivity(intent);
+            }
+        });
 
+        profileURL = profileURL + creator_id;
+
+        //Profile Loader
+        getLoaderManager().initLoader(1, null, profileLoader);
+
+        //getLoaderManager().initLoader(2,null, heritageItemLoader);
 
     }
+
+
+    private LoaderManager.LoaderCallbacks<ArrayList<ProfilePage>> profileLoader
+            = new LoaderManager.LoaderCallbacks<ArrayList<ProfilePage>>() {
+
+
+        public Loader<ArrayList<ProfilePage>> onCreateLoader(int i, Bundle bundle) {
+            Log.v("LoaderProfilePage", "hello");
+            return new ProfilePageLoader(HeritageItemViewActivity.this, profileURL);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<ArrayList<ProfilePage>> loader, ArrayList<ProfilePage> profilePages) {
+            if (profilePages == null) return;
+
+            if (!profilePages.get(0).getPhoto().equals("-1")) {
+                photo = (ImageView) findViewById(R.id.guest_profile_pict);
+                String imageUri = "http://" + profilePages.get(0).getPhoto();
+                Log.v("Uploadtag", imageUri);
+                Picasso.with(getBaseContext()).load(imageUri).into(photo);
+                // String imageUri = baseURL + profilePages.get(0).getPhoto();
+
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<ArrayList<ProfilePage>> loader) {
+
+        }
+
+    };
+
+    /*
+    public void updateUi(ArrayList<HeritageItem> heritageItems) {
+        itemAdapter.clear();
+        if (heritageItems != null || heritageItems.size() > 0) {
+            itemAdapter.addAll(heritageItems);
+        }
+    }
+
+    //WILL BE IMPLEMENT SOON
+    private LoaderManager.LoaderCallbacks<ArrayList<HeritageItem>> heritageItemLoader
+            = new LoaderManager.LoaderCallbacks<ArrayList<HeritageItem>>() {
+
+            public Loader<ArrayList<HeritageItem>> onCreateLoader(int i, Bundle bundle) {
+                return new HeritageItemListFragment();
+
+            }
+
+            @Override
+            public void onLoadFinished(Loader<ArrayList<HeritageItem>> loader, ArrayList<HeritageItem> heritageItems) {
+                if (heritageItems == null) return;
+                updateUi(heritageItems);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<ArrayList<HeritageItem>> loader) {
+                itemAdapter.clear();
+            }
+
+
+    }; */
+
+
 
 }
