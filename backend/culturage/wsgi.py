70835fec688ade20f7989c515849a9bfe7bc6e71
kuzdogan <kaanuzdogan@hotmail.com>
2017-11-18 23:42:28 +0300

    Revert the commit causing merge problems with master
    Revert "Revert "Merge branch 'master' into frontend""
    
    This reverts commit 7a615e0b6186a0e979694d5997d7cc74f692ef15.

--- backend/culturage/wsgi.py
+++ backend/culturage/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "culturage.settings")
+
+sys.path.append('/var/www')
+sys.path.append('/var/www/bounswe2017group11/backend/')
 
 application = get_wsgi_application()
