df43b12ac740f7e64b6746b1da02204c85bbeb6d
halilkalkan <halilkalkan@gmail.com>
2017-12-02 23:26:52 +0300

    Multi image support in item page

--- frontend/src/app/pages/Item/components/Body.jsx
+++ frontend/src/app/pages/Item/components/Body.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../utils/mycss.css';
+import { Carousel } from 'react-responsive-carousel';
+import styles from '../utils/carousel.min.css'
 
 
 class Body extends React.Component {
@@ -18,16 +20,32 @@ class Body extends React.Component {
                })}
       			<hr />
 
-               {(this.props.item.medias != "") ? (this.props.item.medias[0].extension == "youtube") && 
+
+               {(this.props.item.medias != "") ? (this.props.item.medias[0].extension == "youtube") ? 
          <div class="embed-responsive embed-responsive-16by9">
 
-               <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> : 
+               <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> :
+				(this.props.item.medias[0].extension == "jpg") &&
+				<Carousel>
+                <div>
+                    <img src={this.props.item.featured_img} />
+                </div>
+
+				{this.props.item.medias.map(function(media){ return (
+
+                <div>
+                    <img src={media.file_url} />
+                </div>
+            	 );})}
+            	 </Carousel>
+
+               : 
             	<div class="img">
             	<img src={this.props.item.featured_img} class="cover"/>
-            	</div> }                    
+            	</div> }                
             	<div class="jumbotron">
             		<p>{this.props.item.description}</p>
-            		
+    
             	</div>
             </div>	
       } else {
