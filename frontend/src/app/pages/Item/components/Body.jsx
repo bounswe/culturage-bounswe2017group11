e473f1b4738b49fb124e4e6b1fb081920d1a0d33
halil <halilkalkan@github.com>
2017-12-02 21:46:11 +0300

    Bug fix in item page

--- frontend/src/app/pages/Item/components/Body.jsx
+++ frontend/src/app/pages/Item/components/Body.jsx
@@ -17,7 +17,8 @@ class Body extends React.Component {
                      return <a href={"/searchTag/" + data.name}><span class="badge badge-info">#{data.name}</span></a>
                })}
       			<hr />
-               {(this.props.item.medias[0].extension == "youtube") ? 
+
+               {(this.props.item.medias != "") ? (this.props.item.medias[0].extension == "youtube") && 
          <div class="embed-responsive embed-responsive-16by9">
 
                <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> : 
