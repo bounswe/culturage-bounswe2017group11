50baf901ea09c44f0be42fecf202a6acc1d62a88
otugen <nhlezgiyctrk@gmail.com>
2017-12-06 20:30:32 +0300

    Added ItemTestCase: item_rate

--- backend/base/tests.py
+++ backend/base/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from  .models import Item, Comment
+from  .models import Item, Comment, UserRatedItem
 from django.contrib.auth.models import User
 # Create your tests here.
 
@@ -7,13 +7,14 @@ class ItemTestCase(TestCase):
     def setUp(self):
         tester = User.objects.create(first_name="tester")
         item = Item.objects.create(name="test item",description="item to be used on the test purposes",
-                            rate=1, created_by=tester)
+                                 created_by=tester)
         comment = Comment.objects.create(text="test item comment", related_item=item, written_by=tester)
-
-    def test_item_creat(self):
+        usrRateItm = UserRatedItem.objects.create(rate=5,user=tester,item=item)
+    def test_item_create(self):
         item = Item.objects.get(name="test item")
+        item.calculateRate()
         self.assertEqual(item.__str__(),"test item")
-        self.assertEqual(item.rate, 1)
+        self.assertEqual(item.rate, 5)
         self.assertEqual(item.description, "item to be used on the test purposes")
 
     def test_item_comment(self):
@@ -21,6 +22,13 @@ class ItemTestCase(TestCase):
         tester = User.objects.get(first_name="tester")
         commenters = item.get_commenters()
         self.assertEqual(commenters[0], tester.id)
+
+    def test_item_rate(self):
+        item = Item.objects.get(name="test item")
+        tester = User.objects.get(first_name="tester")
+        raters = item.get_raters()
+        self.assertEqual(raters[0], tester.id)
+
 
     def test_item_delete(self):
         item = Item.objects.get(name="test item")
