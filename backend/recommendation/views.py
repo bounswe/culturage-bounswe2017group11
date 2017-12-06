d87094876a96cf433d713ef74aab772e252d9844
A Koksal <koksalyazilim@gmail.com>
2017-12-06 18:12:36 +0300

    Recommendation for User is finished. It is based on user's likes.

--- backend/recommendation/views.py
+++ backend/recommendation/views.py
@@ -7,7 +7,8 @@ from base.serializers import NewsfeedSerializer
 from base.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-#import gensim
+import gensim
+import math
 import os
 
 class UserRecommendation(APIView):
@@ -15,10 +16,66 @@ class UserRecommendation(APIView):
     API endpoint that gives recommendation for user.
     """
 	def get(self, request):
+		model = gensim.models.KeyedVectors.load(os.path.join(settings.BASE_DIR,"50k"))
 		user = request.user
 		items = Item.objects.order_by('-created_at').filter(rated_item__user=user)
 		items = NewsfeedSerializer.setup_eager_loading(items)  # Set up eager loading to avoid N+1 selects
-		serializer = NewsfeedSerializer(items, many=True, context={'request': request})
+		print("ssssss",len(items))
+		tagsDict = {}
+		for item in items:
+			print("------------")
+			print("ITEM BU:",item)
+			tags = item.tags.all()
+			for tag in tags:
+				for tagPart in tag.name.split():
+					if tagPart in model:
+						if tagPart in tagsDict:
+							tagsDict[tagPart] += 1
+						else:
+							tagsDict[tagPart] = 1
+		
+		sortedTags = sorted(tagsDict.items(), key=lambda x:x[1], reverse = True)
+		
+		allItems = Item.objects.order_by('-created_at').filter()
+		returnedItems = []
+		scores = []
+
+		for item in allItems:
+			if item in items:
+				continue
+			avSim = 0
+			count = 0
+
+			for tag in item.tags.all():
+				#print("-----------",tag)
+				for tagPart in tag.name.split():
+					#print("--------",tagPart)
+					for key, value in sortedTags:
+						if tagPart in model and key in model:
+							print(key,"--------",tagPart)
+							avSim = avSim + model.similarity(key,tagPart)*math.log(value+1)
+							count = count + 1
+							print("sim:",avSim,"count:",count)
+			print("*****",count)
+			if count==0:
+				avSim = 0
+			else:
+				avSim = avSim/count
+
+			print("avSim between: ",item,avSim)
+			returnedItems.append(item)
+			scores.append(avSim)
+			
+
+
+		returnedItems = [x for _, x in sorted(zip(scores,returnedItems), key = lambda pair: pair[0], reverse = True)]	
+		
+		for key, value in sortedTags:
+			print(key,value)
+
+		
+		returnedItems = returnedItems[0:15]
+		serializer = NewsfeedSerializer(returnedItems, many=True, context={'request': request})
 		return Response(serializer.data)
 
 class ItemRecommendation(APIView):
