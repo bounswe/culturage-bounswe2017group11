2341fddf051a8cce7242a248c77893d975e713b4
kuzdogan <kaanuzdogan@hotmail.com>
2017-12-07 00:59:13 +0300

    Remove slider arrows, fix video in slider

--- frontend/src/app/pages/Item/components/Body.js
+++ frontend/src/app/pages/Item/components/Body.js
@@ -35,11 +35,11 @@ class Body extends React.Component {
             <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> 
           </div>
           <div class="item-image-tab" style={{display:(!showVideoTab ? 'block' : 'none')}}>
-            <Carousel showStatus={false}>
+            <Carousel showStatus={false} showArrows={false}>
               <div>
                   <img src={this.props.item.featured_img} />
               </div>
-              {this.props.item.medias.map(function(media){ return (
+              {this.props.item.medias.slice(1).map(function(media){ return (
                 <div>
                     <img src={media.file_url} />
                 </div>
@@ -51,7 +51,7 @@ class Body extends React.Component {
         :
 				(this.props.item.medias[0].mediaType == "image") &&
         // Show images only        
-				<Carousel showStatus={false}>
+				<Carousel showStatus={false} showArrows={false}>
           <div>
               <img src={this.props.item.featured_img} />
           </div>
