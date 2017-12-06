1ce07cee8d9c0a029cf79334b5b262144586a674
kuzdogan <kaanuzdogan@hotmail.com>
2017-12-06 22:04:19 +0300

    Enable slider buttons

--- frontend/src/app/pages/Item/components/Body.js
+++ frontend/src/app/pages/Item/components/Body.js
@@ -35,7 +35,7 @@ class Body extends React.Component {
             <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> 
           </div>
           <div class="item-image-tab" style={{display:(!showVideoTab ? 'block' : 'none')}}>
-            <Carousel showArrows={false} showStatus={false} showIndicators={false}>
+            <Carousel showStatus={false}>
               <div>
                   <img src={this.props.item.featured_img} />
               </div>
@@ -51,7 +51,7 @@ class Body extends React.Component {
         :
 				(this.props.item.medias[0].mediaType == "image") &&
         // Show images only        
-				<Carousel showArrows={false} showStatus={false} showIndicators={false}>
+				<Carousel showStatus={false}>
           <div>
               <img src={this.props.item.featured_img} />
           </div>
