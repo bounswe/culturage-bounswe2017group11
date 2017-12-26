cbcd29dc3978f28cde47f81e08a9cc09ff69c2e7
rizaozcelik <rizaozcelik96@gmail.com>
2017-12-26 17:46:50 +0300

    Surprise is implemented.

---
+++ android/Culturage/app/src/main/java/com/culturage/oceans_eleven/culturage/baseClasses/MyApplication.java
@@ -0,0 +1,35 @@
+package com.culturage.oceans_eleven.culturage.baseClasses;
+
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.onesignal.OneSignal;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+}
