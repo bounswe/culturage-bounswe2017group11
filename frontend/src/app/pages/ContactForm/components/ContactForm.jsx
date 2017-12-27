351ac2a54a2cce320118bb8b970b42c3e90efb3a
ptpam <pampam1>
2017-12-27 22:26:32 +0300

    date format & location info

--- frontend/src/app/pages/ContactForm/components/ContactForm.jsx
+++ frontend/src/app/pages/ContactForm/components/ContactForm.jsx
@@ -15,14 +15,23 @@ class ContactForm extends React.Component {
             imagePreviewUrl: ' ',
             name: '',
             description: '',
-            location: '',
+            location: {
+                name: "",
+                longtitude: null,
+                latitude: null,
+            },
             day:'00',
             month:'00',
             year:'0000',
+            day2:'00',
+            month2:'00',
+            year2:'0000',
             image: '',
             tags: [],
             tag: '',
             isChecked: false,
+            isRepetitive: false,
+            isRange: false,
         }
         this.handleSelectedChange = this.handleSelectedChange.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
@@ -31,12 +40,20 @@ class ContactForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleClear = this.handleClear.bind(this);
         this.toggleChange = this.toggleChange.bind(this);
+        this.repetitiveChange = this.repetitiveChange.bind(this);
+        this.rangeChange = this.rangeChange.bind(this);
 
         this.handleYearChange = this.handleYearChange.bind(this);
 
         this.handleMonthChange = this.handleMonthChange.bind(this);
 
         this.handleDayChange = this.handleDayChange.bind(this);
+
+        this.handleYear2Change = this.handleYear2Change.bind(this);
+
+        this.handleMonth2Change = this.handleMonth2Change.bind(this);
+
+        this.handleDay2Change = this.handleDay2Change.bind(this);
     }
 
     toggleChange = () => {
@@ -44,6 +61,8 @@ class ContactForm extends React.Component {
         isChecked: !this.state.isChecked,
       });
     }
+    repetitiveChange = () => {this.setState({isRepetitive: !this.state.isRepetitive});}
+    rangeChange = () => {this.setState({isRange: ! this.state.isRange});}
 
     handleChangeTags(tags) {this.setState({tags});}
 
@@ -79,6 +98,12 @@ class ContactForm extends React.Component {
 
     handleDayChange(event){event.preventDefault();  this.setState({day: event.target.value});};
 
+    handleYear2Change(event){event.preventDefault();this.setState({year2: event.target.value});};
+
+    handleMonth2Change(event){event.preventDefault();  this.setState({month2: event.target.value});};
+
+    handleDay2Change(event){event.preventDefault();  this.setState({day2: event.target.value});};
+
     handleImageChange(event){event.preventDefault();this.setState({image: event.target.value});};
     handleTagsChange(event){event.preventDefault();this.setState({tags: event.target.value});};
     handleSelectedChange(event) {this.setState({selectedValue: event.target.value});}
@@ -96,7 +121,7 @@ class ContactForm extends React.Component {
               "latitude": lat,
             }
             console.log(loc)
-            this.setState({location: loc})      
+            this.setState({location: loc})
         })
     }
 
@@ -107,10 +132,27 @@ class ContactForm extends React.Component {
 
       var myHeaders = new Headers();
       var yearValue = this.state.year;
+      var yearValue2 = this.state.year2;
+
       if(this.state.isChecked){
         yearValue = '-'+ yearValue;
+        if(this.state.isRange){
+          yearValue2 = '-'+ yearValue2;
+        }
       }
+
       var dateUpload = yearValue + '-' + this.state.month + '-' + this.state.day;
+      var dateUpload2 = yearValue2 + '-' + this.state.month2 + '-' + this.state.day2;
+      if(this.state.isRepetitive){
+        dateUpload = dateUpload + '~';
+        if(this.state.isRange){
+          dateUpload2 = dateUpload2 + '~';
+        }
+      }
+
+      if(!this.state.isRange){
+        dateUpload2 = null;
+      }
 
       console.log(dateUpload);
 
@@ -118,7 +160,10 @@ class ContactForm extends React.Component {
         "name" : this.state.name,
         "description": this.state.description,
         "location": this.state.location,
-        "date": dateUpload,
+        "date": {
+            "start": dateUpload,
+            "end": dateUpload2,
+        },
         "image": this.state.imagePreviewUrl,
         "tags": this.state.tags
       };
@@ -223,6 +268,28 @@ class ContactForm extends React.Component {
                         BC
                       </label>
                     </div>
+                    <div className="col-lg-3">
+                      <label>
+                        <input type="checkbox"
+                          checked={this.state.isRepetitive}
+                          onChange={this.repetitiveChange}
+                        />
+                        Repetitive
+                      </label>
+                    </div>
+                    <div className="col-lg-3">
+                      <label>
+                        <input type="checkbox"
+                          checked={this.state.isRange}
+                          onChange={this.rangeChange}
+                        />
+                        Time Interval
+                      </label>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className="col-lg-3 control-label"></label>
+                    <label className="col-lg-1 control-label">Start:</label>
                     <div className="col-lg-2">
                       <input className="form-control"
                         type="number"
@@ -262,6 +329,47 @@ class ContactForm extends React.Component {
                   </div>
 
                   <div className="form-group">
+                    <label className="col-lg-3 control-label"></label>
+                    <label className="col-lg-1 control-label">End:</label>
+                    <div className="col-lg-2">
+                      <input className="form-control"
+                        type="number"
+                        name="day"
+                        ref="day"
+                        min="1"
+                        max="31"
+                        placeholder="DD"
+                        value={ this.state.subject }
+                        onChange={ this.handleDay2Change }
+                      required />
+                    </div>
+                    <div className="col-lg-2">
+                      <input className="form-control"
+                        type="number"
+                        name="month"
+                        ref="month"
+                        min="1"
+                        max="12"
+                        placeholder="MM"
+                        value={ this.state.subject }
+                        onChange={ this.handleMonth2Change }
+                      required />
+                    </div>
+                    <div className="col-lg-2">
+                      <input className="form-control"
+                        type="number"
+                        name="year"
+                        ref="year"
+                        min="0"
+                        max="3000"
+                        placeholder="YYYY"
+                        value={ this.state.subject }
+                        onChange={ this.handleYear2Change }
+                      required />
+                    </div>
+                  </div>
+
+                  <div className="form-group">
                     <label className="col-lg-3 control-label">Tags:</label>
                     <div className="col-lg-8">
                       <TagsInput value={this.state.tags} onChange={this.handleChangeTags} />
@@ -282,7 +390,7 @@ class ContactForm extends React.Component {
                       <button className="btn btn-primary custom margin-left"
                         onClick={ this.handleClear }>Clear</button>
                     </div>
-                  </div>              
+                  </div>
                 </form>
             </div>
           </div>
