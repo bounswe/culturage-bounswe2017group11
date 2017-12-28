b0303b37f90e4a7dadf9dda8d38cb88c43fcb8a3
kuzdogan <kaanuzdogan@hotmail.com>
2017-12-28 15:01:06 +0300

    Added Annotations

--- frontend/src/app/pages/Item/components/Body.js
+++ frontend/src/app/pages/Item/components/Body.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import '../utils/mycss.css';
 import { Carousel } from 'react-responsive-carousel';
-import styles from '../utils/carousel.min.css'
+import styles from '../utils/carousel.min.css';
+import $ from 'jquery';
 
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+var token = getCookie('token');
+if(token == null){
+    token = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImhhbGlsa2Fsa2FuOTVAZ21haWwuY29tIiwidXNlcl9pZCI6MywiZXhwIjoxNTM3OTA1NDQ0LCJ1c2VybmFtZSI6ImhhbGlsIn0.hV0dPW3IsrqynXjwiycc5s25dtaReLP6J446soiwU2Y"
+}else{
+    token = "JWT " + token;
+}
 
 class Body extends React.Component {
    constructor(props){
       super(props);
       this.state = { showVideoTab: true};
    }
+   componentDidMount(){
+    $(document).ready(function() {
+      $('.carousel-slider').click(function(e) {
+        var offset = $(this).offset();
+        var imgx = e.pageX - offset.left;
+        imgx = parseInt(imgx);
+        var imgy = e.pageY - offset.top;
+        imgy = parseInt(imgy);
+        var imgid = $(e.target).find('img').attr('image-id');
+        console.log("IMAGE ID " + imgid);
+        var annotation = prompt("Add annotation", "");
+        var body = {
+                "text": annotation,
+                "x" : imgx,
+                "y" : imgy,
+                "w": 1,
+                "h": 1
+              };
+        if(annotation){
+          //alert(annotation);           
+          $.ajax({
+              url: "http://52.90.34.144:85/api/medias/" + imgid + "/annotations/",
+              type: "post",
+              headers: {
+                "Content-Type" : "application/json",
+                "Accept" : "application/json",
+                "Authorization" : token
+              },              
+              data: JSON.stringify(body),
+              success: function(res){
+                console.log(res);
+                window.location.reload();
+              },
+              error: function(err) {
+                console.log(err);
+                alert("Could not add annotation");
+              }
+
+          });
+        }
+      });
+    });
+  }
 
 
    render() {
@@ -38,9 +93,22 @@ class Body extends React.Component {
             <Carousel showStatus={false} showArrows={true} emulateTouch={true}>
             
               {this.props.item.medias.slice(1).map(function(media){ return (
-                <div>
-                    <img src={media.file_url} />
-                </div>
+                <div class="img-wrapper">
+                <img src={media.file_url} image-id={media.id}/>
+                {media.annotations.map(function(anno){ 
+                  var annoid = "pin-" + anno.id;
+                  var style = {
+                    top: anno.y,
+                    left: anno.x
+                  }                  
+                  return (
+                    <div id={annoid} class="box" style={style}>
+                      <div class="pin-text">
+                        <h3>{anno.text}</h3>
+                      </div>
+                    </div>   
+                )})}             
+            </div>
                )}
               )}
             </Carousel>
@@ -51,15 +119,28 @@ class Body extends React.Component {
         // Show images only        
 				<Carousel showStatus={false} showArrows={true} emulateTouch={true}>
   				{this.props.item.medias.map(function(media){ return (
-            <div>
-                <img src={media.file_url} />
+            <div class="img-wrapper">
+                <img src={media.file_url} image-id={media.id}/>
+                {media.annotations.map(function(anno){ 
+                  var annoid = "pin-" + anno.id;
+                  var style = {
+                    top: anno.y,
+                    left: anno.x
+                  }                  
+                  return (
+                    <div id={annoid} class="box" style={style}>
+                      <div class="pin-text">
+                        <h3>{anno.text}</h3>
+                      </div>
+                    </div>   
+                )})}             
             </div>
       	   )}
           )}
         </Carousel>
          : 
       	<div class="img">
-      	<img src={this.props.item.featured_img} class="cover"/>
+      	<img src={this.props.item.featured_img} class="cover" />
       	</div> }
 
 
