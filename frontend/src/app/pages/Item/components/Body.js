24950050c1546de37f9341e231b25ac9d5777b83
halilkalkan <halilkalkan95@gmail.com>
2017-12-28 13:56:35 +0300

    Slide and arrow added to imageslider

--- frontend/src/app/pages/Item/components/Body.js
+++ frontend/src/app/pages/Item/components/Body.js
@@ -35,10 +35,8 @@ class Body extends React.Component {
             <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.item.medias[0].url.substring(32)} frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div> 
           </div>
           <div class="item-image-tab" style={{display:(!showVideoTab ? 'block' : 'none')}}>
-            <Carousel showStatus={false} showArrows={false}>
-              <div>
-                  <img src={this.props.item.featured_img} />
-              </div>
+            <Carousel showStatus={false} showArrows={true} emulateTouch={true}>
+            
               {this.props.item.medias.slice(1).map(function(media){ return (
                 <div>
                     <img src={media.file_url} />
@@ -51,7 +49,7 @@ class Body extends React.Component {
         :
 				(this.props.item.medias[0].mediaType == "image") &&
         // Show images only        
-				<Carousel showStatus={false} showArrows={false}>
+				<Carousel showStatus={false} showArrows={true} emulateTouch={true}>
   				{this.props.item.medias.map(function(media){ return (
             <div>
                 <img src={media.file_url} />
